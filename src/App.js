ef83f2396c400fdbe1367ca5377eafae510be1ed
ranjankumar9 <107936455+ranjankumar9@users.noreply.github.com>
2023-02-21 13:11:19 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from './Images/projectlogo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Project name :-  ShopOffer.com</h1>
+      <img src={logo} width='800' alt='Projectlogo'/>
+    </div>
+  );
+}
+
+export default App;
