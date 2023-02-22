46fe2f1b3813b98e661dcf5aea018afed89df74e
deep1049 <103635801+deep1049@users.noreply.github.com>
2023-02-22 19:37:11 +0530

    day-2 topNavbar completed

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import logo from './Images/projectlogo.png'
+import "./App.css";
+import NavbarMain from "./components/NavbarMain";
+import logo from "./Images/projectlogo.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>Project name :-  ShopOffer.com</h1>
-      <img src={logo} width='800' alt='Projectlogo'/>
+      <NavbarMain />
+      <h1>Project name :- ShopOffer.com</h1>
+      <img src={logo} width="800" alt="Projectlogo" />
     </div>
   );
 }
