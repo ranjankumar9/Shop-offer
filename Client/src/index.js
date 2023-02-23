d280edc5e5535a2dc68a70f879caaf5332db27aa
ranjankumar9 <107936455+ranjankumar9@users.noreply.github.com>
2023-02-23 12:36:31 +0530

    ad

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,23 +7,20 @@ import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store'
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
->>>>>>> e5f6249891cf5351a0d5898452533015432e9351
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
+  <BrowserRouter>
+  <ChakraProvider>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </ChakraProvider>
+</BrowserRouter>
 
-    <ChakraProvider>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </ChakraProvider>
 
 );
 
@@ -31,11 +28,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-=======
-  <BrowserRouter>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </BrowserRouter>
-);
->>>>>>> e5f6249891cf5351a0d5898452533015432e9351
+
+
