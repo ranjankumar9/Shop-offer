45fbcd51cbe4705a19a3065fd22c56a52b839afc
ranjankumar9 <107936455+ranjankumar9@users.noreply.github.com>
2023-02-24 11:37:16 +0530

    a

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,15 +8,15 @@ import Checkout from './Components/Checkout';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       {/* <Cart /> */}
-      <Checkout />
-=======
+      {/* <Checkout />
+
       <HomePage />
-      <Cart />
->>>>>>> main
+      <Cart /> */}
+
       {/* <Admin /> */}
-      {/* <AllRoutes /> */}
+      <AllRoutes />
       {/* <h1>Project name :- ShopOffer.com</h1>
       <img src={logo} width="800" alt="Projectlogo" /> */}
     </div>
