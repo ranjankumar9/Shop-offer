b29632c0a9cf55de3b713ea19eb24c658ad44a3d
Gaurav Suthar <gauravsuthar309@gmail.com>
2023-02-24 16:50:06 +0530

    fixed conflicts

---
+++ Client/src/Components/sideBarhover/homekichenhover.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+const Homekichenhover = () => {
+  return (
+    <div className="men-navbar">
+      {/* column 1 */}
+      <div className="column">
+        <div className="section">
+          <h2>KITCHEN APPLIANCES</h2>
+          <div className="item">Juicer Mixer Grinders</div>
+          <div className="item">Water Purifiers</div>
+          <div className="item"> Gas Stoves & Hobs</div>
+          <div className="item">Chimneys</div>
+          <div className="item">Induction Cooktops</div>
+          <div className="item">Air Fryers</div>
+          <div className="item">Electric Cookers</div>
+          <div className="item">Sandwich Makers</div>
+          <div className="item">Choppers & Blenders</div>
+          <div className="item">Kettles & Coffee Makers</div>
+          <div className="item">Roti Makers</div>
+        </div>
+        <div className="section">
+          <h2>FURNITURE</h2>
+          <div className="item">Storage & Display</div>
+
+          <div className="item">Bean Bags</div>
+          <div className="item">Tables & Desks</div>
+        </div>
+      </div>
+      {/* column 2 */}
+      <div className="column">
+        <div className="section">
+          <h2>HOME FURNISHING</h2>
+          <div className="item">Bed Linen</div>
+          <div className="item">Bath Linen</div>
+          <div className="item">Blankets & Quilts</div>
+
+          <div className="item">Curtains & Accessories</div>
+          <div className="item">Mattresses</div>
+        </div>
+        <div className="section">
+          <h2>KITCHENWARE</h2>
+          <div className="item">Cookware & Bakeware</div>
+          Dining & Serving
+          <div className="item">Kitchen Storage</div>
+          <div className="item">Kitchen Tools</div>
+          <div className="item">Pressure Cookers</div>
+          <div className="item">Water Bottles</div>
+          <div className="item">Tea & Coffeeware</div>
+          <div className="item">Bar Accessories</div>
+        </div>
+      </div>
+      {/* column 3 */}
+      <div className="column">
+        <div className="section">
+          <h2>HOME IMPROVEMENT</h2>
+          <div className="item">Home Utility</div>
+          <div className="item">Cleaning Mops</div>
+
+          <div className="item">Plants & Gardening</div>
+          <div className="item">Ironing Boards</div>
+          <div className="item">Home Cleaning</div>
+          <div className="item">Laundry Accessories</div>
+        </div>
+        <div className="section">
+          <h2>TOOLS & HARDWARE</h2>
+          <div className="item">Power & Hand Tools</div>
+          <div className="item">Bathroom Accessories</div>
+          <div className="item">Electrical</div>
+
+          <div className="item">Safes Lock & Door Fitting</div>
+          <div className="item">Sanitaryware</div>
+          <div className="item">Taps & Showers</div>
+        </div>
+      </div>
+
+      {/* column 4 */}
+      <div className="coloumn">
+        <img src="https://n1.sdlcdn.com/imgs/i/1/r/GM_28oct-e8cd1.jpg" />
+      </div>
+    </div>
+  );
+};
+
+export default Homekichenhover;
