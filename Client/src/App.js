e1ea34c1f3ff057e864e8b2cadc4110be6481114
ranjankumar9 <107936455+ranjankumar9@users.noreply.github.com>
2023-02-25 15:26:38 +0530

    as

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,13 @@
 
 import "./App.css";
+import CategoryPage from "./Components/CategoryPage";
 import AllRoutes from "./routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes />
+      {/* <AllRoutes /> */}
+      <CategoryPage />
     </div>
   );
 }
