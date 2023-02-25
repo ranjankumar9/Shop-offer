1dd16d35791de00ba129aa8a5fad4ea14aab997f
deep1049 <103635801+deep1049@users.noreply.github.com>
2023-02-25 17:21:51 +0530

    homepage with footer completed

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Carousel from "./Carousel/Carousel";
-import CarouselBottom from "./Carousel/CarouselBottom";
-import NavbarMain from "./NavbarMain";
-import Sidebar from "./Sidebar";
+import Carousel from "../Components/Carousel/Carousel";
+import CarouselBottom from "../Components/Carousel/CarouselBottom";
+import Footer from "../Components/Layout/Footer";
+import NavbarMain from "../Components/NavbarMain";
+import Sidebar from "../Components/Sidebar";
 
 const HomePage = () => {
   return (
     <div style={{ backgroundColor: "whitesmoke" }}>
-      <NavbarMain />
       <div className="sidebar-carousel-wrapper">
         <Sidebar />
         <div className="carousel-wrapper">
